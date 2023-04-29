daf42964b3e0aee951c4f97caaaaaaa2caa8c8ea
chintupr <priyaranjansahoo49@gmail.com>
2023-04-29 20:15:03 +0530

    priyaranjan

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     
     <div className="container">
+      
       <div>
               <img src={img1} alt="" width="600px" height="200px"/>
        <label>
