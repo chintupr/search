ff119de4e6bbc71b5bddb11e36409dff84b4400a
chintupr <priyaranjansahoo49@gmail.com>
2023-04-30 08:36:09 +0530

    priyaranjan

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import img1 from './images/g.jpg'
+import img1 from './images/googlelogo_color_272x92dp.png'
 import  './App.css'
+import './style.css'
 export default function App() {
+  
   return (
     
     <div className="container">
-      
       <div>
-              <img src={img1} alt="" width="600px" height="200px"/>
+              <img src={img1} alt="" width="350px" height="100px"/>
        <label>
         <input type="text"  />
-        <button type="submit">Search</button>
        </label>
        <br/><button type="submit">Google Search</button>
-       <button type="submit">I'M Feeling lucky</button>
+       <button type="submit">I'm Feeling lucky</button>
        </div>
-    </div>
+       </div>
+    
     
     
 
